1634093aa37cd0472f7620fd7dcecf275c7167c8
Andrea Yao <andreayao@HRGAdmins-MBP.home>
2021-06-25 09:26:31 -1000

    Created a list of numbers

--- problem1/problem1.js
+++ problem1/problem1.js
@@ -4,4 +4,8 @@ function helloWorld() {
     console.log('Hello World');
 }
 
-helloWorld();
+helloWorld()
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
