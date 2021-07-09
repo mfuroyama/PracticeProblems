2abbb8abf6a2173951e418c233cea53b674ae989
Andrea Yao <andreayao@HRGAdmins-MBP.home>
2021-07-08 16:03:27 -1000

    Added Error! value

--- problem4/problem4.js
+++ problem4/problem4.js
@@ -5,12 +5,14 @@ var fahrenheitValue = document.getElementById('fahrenheit-value');
 // ==== Implement your code here! ====
 function convert(celsiusValue) {
     console.log(celsiusValue);
-    
+
     fahrenheitValue.innerHTML = (celsiusValue*9/5)+32;
-    
+
+    if (isNaN(celsiusValue)) {
+        return fahrenheitValue.innerHTML = 'Error!';
+    }
 }
-    
-       
+
 button.onclick = function() {
     convert(celsiusInput.value);
 }
