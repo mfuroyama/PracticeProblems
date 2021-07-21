5b2be2f0ccf78224e90787f36418422ea42af178
Mike Furoyama <mfuroyama@hawaiirg.com>
2021-07-21 11:55:54 -1000

    Update the files with notes and comments

--- problem4/problem4.js
+++ problem4/problem4.js
@@ -1,14 +1,27 @@
 var button = document.getElementById('convert-button');
 var celsiusInput = document.getElementById('celsius-value');
-var fahrenheitValue = document.getElementById('fahrenheit-value');
-var kelvinValue = document.getElementById('kelvin-value');
-var radio = document.getElementById('fahrenheit-value');
-var radio = document.getElementById('kelvin-value');
+
+// You can test to see whether or not a radio button is selected using the `.checked` value. So
+// for example, if you want to see if the `fahrenheitRadio` radio button is selected, you can 
+// use something like this:
+//   if (fahrenheitRadio.checked === true) {
+//       ....
+//   }
+var fahrenheitRadio = document.getElementById('fahrenheit-radio');
+var kelvinRadio = document.getElementById('kelvin-radio');
+
+var convertedValue = document.getElementById('converted-value');
 
 // ==== Implement your code here! ====
-function convert(celsiusValue) 
-    
+function convert(celsiusValue) {
+    // Here, check to see which radio button is selected, then use the appropriate conversion
+    // formula, and `return` that value.
+}
     
 button.onclick = function() {
-    convert(celsiusInput.value);
+    // Don't forget to re-add the check for a non-numeric "celsiusInput.value" here!
+
+    var value = convert(celsiusInput.value);
+
+    convertedValue.innerHTML = value;
 }
