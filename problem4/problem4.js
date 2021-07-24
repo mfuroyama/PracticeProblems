9e8db2e3c1f33d14a49c70918d65ce413839af9e
Andrea Yao <andreayao@HRGAdmins-MBP.home>
2021-07-24 12:16:10 -1000

    Included two new conversions using radio buttons

--- problem4/problem4.js
+++ problem4/problem4.js
@@ -1,25 +1,26 @@
 var button = document.getElementById('convert-button');
 var celsiusInput = document.getElementById('celsius-value');
-
-// You can test to see whether or not a radio button is selected using the `.checked` value. So
-// for example, if you want to see if the `fahrenheitRadio` radio button is selected, you can 
-// use something like this:
-//   if (fahrenheitRadio.checked === true) {
-//       ....
-//   }
 var fahrenheitRadio = document.getElementById('fahrenheit-radio');
 var kelvinRadio = document.getElementById('kelvin-radio');
-
 var convertedValue = document.getElementById('converted-value');
 
 // ==== Implement your code here! ====
 function convert(celsiusValue) {
-    // Here, check to see which radio button is selected, then use the appropriate conversion
-    // formula, and `return` that value.
+    
+    if (isNaN(celsiusValue)) {
+        return convertedValue.innerHTML = 'Error!'
+    }
+
+    if (fahrenheitRadio.checked === true) {
+        return convertedValue.innerHTML = (celsiusValue*9/5)+32;
+    }
+
+    if (kelvinRadio.checked === true) {
+        return convertedValue.innerHTML = (celsiusValue*1)+273.15;
+    }
 }
     
 button.onclick = function() {
-    // Don't forget to re-add the check for a non-numeric "celsiusInput.value" here!
 
     var value = convert(celsiusInput.value);
 
