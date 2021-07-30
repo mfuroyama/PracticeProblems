712601a9fafde87fc38b62762afec36c355bda20
Mike Furoyama <mfuroyama@hawaiirg.com>
2021-07-30 07:40:45 -1000

    Add a calculate problem file

---
+++ problem5/calculate.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Create your add and subtract functions here */
+
+
+int main() {
+    int sum = add(2, 4);
+    printf("add: 2 + 4 = %d\n", sum);
+
+    int diff = subtract(4, 2);
+    printf("subtract: 4 - 2 = %d\n", diff);
+}
