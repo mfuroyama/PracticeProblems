75105a72edade7e4990478e80b26721529f26b60
Mike Furoyama <mfuroyama@hawaiirg.com>
2021-08-02 07:48:28 -1000

    Add framework for multiply and divide

--- problem5/calculate.c
+++ problem5/calculate.c
@@ -15,8 +15,14 @@ int main() {
     int sum = add(2, 4);
     printf("add: 2 + 4 = %d\n", sum);
 
-   int diff = subtract(4, 2);
+    int diff = subtract(4, 2);
     printf("subtract: 4 - 2 = %d\n", diff);
+
+    float product = multiply(12.0, 0.25);
+    printf("multiply: 12.0 * 0.25 = %0.1f (should be 3.0)\n", product);
+
+    float quotient = divide(5.0, 0.1);
+    printf("divide: 5.0 / 0.1 = %0.1f (should be 50.0)\n", quotient);
 
     return 0;
 }
